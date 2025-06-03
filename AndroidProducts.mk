ffb652e477e0c88096d392db4a2e305365b9ea20
RocketRakun <138932464+RocketRakun@users.noreply.github.com>
2025-06-03 18:33:51 +0700

    add board

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_X695C.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_X695C-user \
+    twrp_X695C-userdebug \
+    twrp_X695C-eng
